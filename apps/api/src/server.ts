c4c9fb6c9cdc6e7a847b643f84df044808e048e8
yogesh jamatia <jmtygsh@gmail.com>
2026-05-28 00:57:07 +0530

    patch cors

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -18,33 +18,15 @@ const openApiDocument = generateOpenApiDocument(serverRouter, {
   baseUrl: env.BASE_URL.concat("/api"),
 });
 
-// if (env.NODE_ENV !== "prod") {
-//   app.use(
-//     cors({
-//       origin: "*",
-//     }),
-//   );
-// } else {
-const allowedOrigins = [
-  env.FRONTEND_URL,
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) !== -1 || env.NODE_ENV !== "prod") {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
+    origin:
+      env.NODE_ENV === "prod"
+        ? ["https://www.makemyform.in"]
+        : "*",
+    credentials: true,
   }),
 );
-// }
 
 app.use(cookieParser());
 app.use(express.json());
