e3f7de84365ae721e13a5d8beb513345b7d9be58
yogesh jamatia <jmtygsh@gmail.com>
2026-05-28 01:08:23 +0530

    remove BASE_URL from production cors

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin:
       env.NODE_ENV === "prod"
-        ? process.env.BASE_URL || ["https://www.makemyform.in", "https://makemyform.in"]
+        ? ["https://www.makemyform.in", "https://makemyform.in"]
         : "*",
     credentials: true,
   }),
